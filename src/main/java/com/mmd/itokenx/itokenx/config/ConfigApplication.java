e100b55a7610c7cdc439acb5583576f8ac4910f0
laiwurui <m829305546@gmail.com>
2019-09-19 19:13:01 +0800

    add itokenx-service-admin-dev.yml

--- src/main/java/com/mmd/itokenx/itokenx/config/ConfigApplication.java
+++ src/main/java/com/mmd/itokenx/itokenx/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.mmd.itokenx.config;
+package com.mmd.itokenx.itokenx.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
